998e337b58b1837c01546c7fd6445dd54d911902
Olexandr Rizhiy <orizhiy@e1r8p13.unit.ua>
2018-10-21 15:34:35 +0300

    linconflict

--- heuristics.py
+++ heuristics.py
@@ -53,8 +53,40 @@ class Heuristics:
 
         return h
 
-    def linear_manhattan(self):
-        pass
+    def linear_manhattan(self, grid, n, solved_puzzle):
+        vertical_conflicts, horizontal_conflicts = self.linear_conflict(grid, n, solved_puzzle)
+        manhattan_h = self.manhattan(grid, n, solved_puzzle)
+        linear_conflict_h = len(horizontal_conflicts) + len(vertical_conflicts)
+        return manhattan_h + linear_conflict_h
+
+
+
+
+    def linear_conflict(self, grid, n, solved_puzzle):
+        horizontal_conflicts = self.conflict(grid, solved_puzzle)
+
+        vertical_resolved_puzzle = [[] for _ in range(n)]
+        vertical_unsolved_puzzle = [[] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                vertical_resolved_puzzle[j].append(solved_puzzle[i][j])
+                vertical_unsolved_puzzle[j].append(grid[i][j])
+
+        vertical_conflicts = self.conflict(vertical_unsolved_puzzle, vertical_resolved_puzzle)
+        return vertical_conflicts, horizontal_conflicts
+
+    def conflict(self, solved, unsolved):
+        conflicts = []
+        for solved_row, unsolved_row in zip(solved, unsolved):
+            for i in unsolved_row:
+                for j in unsolved_row:
+                    if j in solved_row and i in solved_row and ((solved_row.index(i) != unsolved_row.index(i) and ((unsolved_row.index(i) < unsolved_row.index(j) and solved_row.index(i) > solved_row.index(j)) or (unsolved_row.index(j) < unsolved_row.index(i) and solved_row.index(j) > solved_row.index(i)))) or (solved_row.index(j) != unsolved_row.index(j) and ((unsolved_row.index(j) < unsolved_row.index(i) and solved_row.index(j) > solved_row.index(i)) or (unsolved_row.index(i) < unsolved_row.index(j) and solved_row.index(i) > solved_row.index(j))))):
+                        conflicts.append((i, j))
+        return conflicts
+
+
+
+
 
     def linear_corner_manhattan(self):
         pass
