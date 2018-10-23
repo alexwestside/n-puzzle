6c4b22444724031ac205909bf28207b3edc1a445
alexwestside <sklif.85@gmail.com>
2018-10-23 17:58:50 +0300

    add setup

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import setup, find_packages
 
-packages = find_packages()
-
 setup(
     name='n-puzzle',
-    packages=packages,
+    packages=find_packages(),
     entry_points={'console_scripts': ['npuzzle=src.npuzzle:main'], }
 )
